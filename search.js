3dbf182b34fc8c4e341e41f2c17815f8bbda5fef
Ayush <ayushujjwal@yahoo.com>
2023-08-07 19:56:02 +0530

    areee yaaar...!!

--- search.js
+++ search.js
@@ -1,27 +1,37 @@
 var lock = "An Empty String";
+let select = document.getElementById("Search-select");
+let place_hold = document.getElementById("search-input");
+var java = null;
 
 function search() {
-    let select = document.getElementById("Search-select");
-    var java = select.value;
+    select = document.getElementById("Search-select");
+    java = select.value;
     if (java == "Google") {
         lock = 'https://www.google.com/search?q=';
     } else if (java == "Amazon") {
         lock = 'https://amazon.in/s?k=';
-    } else if (java == "All") {
-        lock = 'https://www.google.com/search?q=';
-        broadcast("Both");
-        lock = 'https://amazon.in/s?k=';
+    } else if (java == "Youtube") {
+        lock = 'https://www.youtube.com/results?search_query=';
+    } else {
+        window.open("https://www.java.com/", '_self');
     }
     broadcast("Normal");
 }
 
 function broadcast(type) {
     let text = document.getElementById("search-input");
-    if (type == "Normal") {
-        var url = lock + '+' + text.value;
-        window.open(url, '_blank').focus;
-    } else {
-        var url = lock + '+' + text.value;
-        window.open(url, '_blank');
-    }
+    var url = lock + '+' + text.value;
+    window.open(url, '_blank').focus;
+}
+
+let OpTiOn = document.querySelector('#Search-select');
+console.log(OpTiOn);
+OpTiOn.onclick() = (Event) => {
+    change();
+};
+
+function change() {
+    java = select.value;
+    document.getElementById("search-input").placeholder = 'Search ' + java;
+    console.log(place_hold);
 }
