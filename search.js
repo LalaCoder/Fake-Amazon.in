c7839a6984ee9775ef1ce8d92e0aa2204c842f91
Ayush <ayushujjwal@yahoo.com>
2023-08-07 15:30:27 +0530

    options

--- search.js
+++ search.js
@@ -1,6 +1,25 @@
 function search() {
-    let a = document.getElementById("search-input");
-    const google = 'https://www.google.com/search?q=';
-    var url = google + '+' + a.value;
-    window.open(url, '_blank').focus;
+    let select = document.getElementById("Search-select");
+    var java = select.value;
+    let search = document.getElementById("search-input");
+    if (java == "Google") {
+        var lock = 'https://www.google.com/search?q=';
+    } else if (java == "Amazon") {
+        var lock = 'https://amazon.in/s?k=';
+    } else if (java == "All") {
+        var lock = 'https://www.google.com/search?q=';
+        broadcast("Both");
+        var lock = 'https://amazon.in/s?k=';
+    }
+    broadcast("Normal");
+}
+
+function broadcast(type) {
+    if (type == "Normal") {
+        var url = lock + '+' + search.value;
+        window.open(url, '_blank').focus;
+    } else {
+        var url = lock + '+' + search.value;
+        window.open(url, '_blank');
+    }
 }
